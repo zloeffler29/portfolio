d75cfbdc4d79cc7200c56cdec764cf7223a7ead6
zloeffler29 <149706911+zloeffler29@users.noreply.github.com>
2023-11-02 12:41:30 -0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* ========================= typing animation ===========================*/
+var typed = new Typed(".typing", {
+    strings: ["","Web Designer", "Full Stack JavaScript Developer","Front-End Web Developer", "Visionary"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop:true
+})
+
+/* ========================= Smooth Scroll ===========================*/
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 2000,
+	speedAsDuration: true
+});
